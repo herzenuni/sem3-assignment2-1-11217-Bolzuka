64a276d956ed22a179f45e2823100ea8be437a8a
Shibaeva Maria <Bolzuka@gmail.com>
2017-12-11 11:21:50 +0300

    Update SORT.py

--- SORT/SORT.py
+++ SORT/SORT.py
@@ -9,20 +9,46 @@
 # (возможно использовать код из лекции по тестированию) и проанализируйте время
 # выполнения сортировки двумя методами с помощью модуля timeit (см. файл example_sort.py
 
-def sort(x):
-  list = []
-  for i in x:
-      i = str(i)
-      s = 0
-      for j in i:
-          s += int(j)**2
-      if 0 == s % 17:
-          list.append(i)
-  return list
+# Практичское задание sort
+# Шибаева Мария Дмитриевна|  ИВТ | 2 курс | 1 подгруппа
+# Задача sort
+#
+# ЧАСТЬ 1
+#
+# Реализуйте два алгоритма сортировки: методом пузырька и быструю сортировку,
+# протестируйте их работу с использованием пакета hypothesis
+# (возможно использовать код из лекции по тестированию) и проанализируйте время
+# выполнения сортировки двумя методами с помощью модуля timeit (см. файл example_sort.py)
+
+# Пузырьек
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1, i, -1):
+            if arr[j] < arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+
+    return arr
+
+# Быстрая сортировка
+def quick_sort(arr):
+    if arr:
+        head, *tail = arr
+        return quick_sort([x for x in tail if x <= head]) + \
+               [head] + \
+               quick_sort([x for x in tail if x > head])
+    return []
 
 
-def main():
-    print(' Result: ', sort(range(1000,10000)))
+if __name__ == "__main__":
+    pass
 
-if __name__ == '__main__':
-    main()
+
+
+import timeit
+
+print("TIMEIT")
+print("bubble_sort : ", end='')
+print(timeit.timeit("bubble_sort([1,2,3,4,5,6,7,8,9])", setup="from __main__ import bubble_sort", number=10))
+print("quick_sort : ", end='')
+print(timeit.timeit("quick_sort([1,2,3,4,5,6,7,8,9])", setup="from __main__ import quick_sort", number=10))
